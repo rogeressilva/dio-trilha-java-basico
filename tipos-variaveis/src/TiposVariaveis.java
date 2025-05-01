23c6830bba30f8790334474fd7e71d4743ccc797
Rógeres <rogeress72@gmail.com>
2025-04-30 21:02:50 -0300

    tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        //String deve ser escrito a primeiro letra em maiusculo//
+        //Serve para escrever textos//
+        String Texto = "texto";
+        System.out.println(Texto);
+
+        //int serve para escrever numerais (mas não fracionais)//
+        int numero = 10;
+        System.out.println(numero);
+
+        //double serve para escrever numerais fracionais//
+        double numeroFracional = 9.50;
+        System.out.println(numeroFracional);
+
+        //para manter um valor para uma variavel, deve-se colocar o final e o nome em maiusculo//
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+
     }
 }
